15cf0dcbe58d4741094a7f517503a84c9a162036
Brahmini2 <chummubrahmini@gmail.com>
2025-03-26 10:08:48 +0530

    Add files via upload

---
+++ sum.py
@@ -0,0 +1,14 @@
+Python 3.13.1 (tags/v3.13.1:0671451, Dec  3 2024, 19:06:28) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 5+3
+8
+>>> 45-3
+42
+>>> 35/3
+11.666666666666666
+>>> 2**24
+16777216
+>>> 2**3
+8
+>>> 2*4
+8
